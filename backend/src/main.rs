034b7a213c8ec5877d2b6e62ed02cc4c6871eeec
Jacob Horton <hortonjacob1@gmail.com>
2023-04-30 10:17:45 +0100

    Bind server to any address

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -42,7 +42,7 @@ async fn main() -> std::io::Result<()> {
             .service(list_players)
             .service(add_game)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
