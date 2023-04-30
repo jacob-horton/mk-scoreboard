8b40406019985766842b19ab152307b28835cf1a
Jacob Horton <hortonjacob1@gmail.com>
2023-04-30 20:00:22 +0100

    Revert back to env for server address

--- frontend/src/data/ip.ts
+++ frontend/src/data/ip.ts
@@ -1,6 +1,3 @@
 export default function getIP() {
-  const withoutSchema = window.location.origin.split("//")[1];
-  const withoutPort = withoutSchema.split(":")[0];
-
-  return withoutPort;
+  return import.meta.env.VITE_SERVERADDR;
 }
