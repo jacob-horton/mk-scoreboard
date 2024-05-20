c9dd6b0f49753bcd32cabf20a4097e74206faa9b
Jacob <hortonjacob1@gmail.com>
2024-05-20 23:38:41 +0100

    Remove old shell file

--- build-containers.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source .env.prod
-
-# Pass through:
-# - Database URL for SQLx to use when building
-# - VITE_SERVERADDR to Vite to compile into the application
-docker-compose build --build-arg DATABASE_URL=$DATABASE_URL --build-arg VITE_SERVERADDR=$VITE_SERVERADDR
